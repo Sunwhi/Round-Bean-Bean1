8d5c2bc346b742c0f03114a403f72edc08c9f65a
Sunwhi <sunwhi123@naver.com>
2024-10-30 00:22:41 +0900

    카메라 플레이어와 같이 이동

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    [SerializeField] private GameObject circle;
+    private Vector3 cameraPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cameraPosition = new Vector3(circle.transform.position.x, circle.transform.position.y + 1, -10);
+        transform.position = cameraPosition;
+    }
+}
