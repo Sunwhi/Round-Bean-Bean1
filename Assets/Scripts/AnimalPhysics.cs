3ba1292c5202cf83269b09af12b8e892156cf594
Sunwhi <80499477+Sunwhi@users.noreply.github.com>
2024-10-31 19:54:05 +0900

    자잘한

--- Assets/Scripts/AnimalPhysics.cs
+++ Assets/Scripts/AnimalPhysics.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class AnimalPhysics : MonoBehaviour
 {
-    [SerializeField] private float force;
+    [SerializeField] private float force = 5.0f;
     Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
@@ -16,11 +16,12 @@ public class AnimalPhysics : MonoBehaviour
     void FixedUpdate()
     {
         if(Input.GetKeyDown(KeyCode.L)) 
-        { 
-            //rb.AddForce(transform.localPosition.y )
+        {
+            rb.AddForce(Vector2.right * force, ForceMode2D.Force);
         }
         if(Input.GetKeyDown(KeyCode.J))
         {
+            rb.AddForce(Vector2.left * force, ForceMode2D.Force);
 
         }
     }
