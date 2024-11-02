760d00553adaac5ed8cdaa20c50984d6fb4fcba2
Sunwhi <sunwhi123@naver.com>
2024-11-02 23:36:49 +0900

    animal addforce로 수정

--- Assets/Scripts/AnimalPhysics.cs
+++ Assets/Scripts/AnimalPhysics.cs
@@ -1,11 +1,18 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class AnimalPhysics : MonoBehaviour
 {
-    [SerializeField] private float force = 5.0f;
+    [SerializeField] private float magnitude = 5.0f;
     Rigidbody2D rb;
+    //private Vector2 direction;
+    //private Vector2 velocity;
+    //private Vector2 acceleration;
+    //private Vector2 force;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -15,14 +22,31 @@ public class AnimalPhysics : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if(Input.GetKeyDown(KeyCode.L)) 
+        if (Input.GetKey(KeyCode.RightArrow))
         {
-            rb.AddForce(Vector2.right * force, ForceMode2D.Force);
-        }
-        if(Input.GetKeyDown(KeyCode.J))
-        {
-            rb.AddForce(Vector2.left * force, ForceMode2D.Force);
+            Vector2 direction = transform.right + (transform.up * (-1f));
+            direction.Normalize();
 
+            rb.AddForce(direction * magnitude, ForceMode2D.Force);
+         }
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            Vector2 direction = (transform.right * (-1f)) + (transform.up * (-1f));    
+            direction.Normalize();
+
+            rb.AddForce(direction * magnitude, ForceMode2D.Force);
         }
     }
+    /*private void rightForce()
+    {
+        //Vector2.right
+        direction = transform.right + (transform.up * (-1f));
+        force = direction * magnitude;
+
+        acceleration = force / rb.mass;
+        velocity += acceleration / Time.deltaTime;
+
+        transform.position += (Vector3)velocity * Time.deltaTime;
+    }*/
+
 }
