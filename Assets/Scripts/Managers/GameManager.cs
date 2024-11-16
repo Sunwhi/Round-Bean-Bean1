e4fc6a36800cb7f1c8b412524788614a9de755fb
Sunwhi <sunwhi123@naver.com>
2024-11-16 15:24:51 +0900

    시간(스코어)표시 구현

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
 /*
  * GameManager 클래스 
  * 싱글톤 이용하여 다른 씬이나 다른 오브젝트에서 GameManager 클래스 속 함수나 변수 공유 가능!
@@ -10,6 +13,8 @@ public class GameManager : MonoBehaviour
     [SerializeField] Collider2D animalCollider;
     public bool isGround = false;
     private bool gameOver;
+
+    public TextMeshProUGUI textTime;  // 시간을 나타내는 텍스트
 
     public static GameManager Instance { get; private set; } // 싱글톤 인스턴스
 
@@ -38,8 +43,19 @@ public class GameManager : MonoBehaviour
     {
         // AnimalCollision.cs에서 isGround가 true가 되었다면 GameOver함수를 호출한다.
         if (isGround) GameOver();
+
+        //textTime에 현재 시간을 표시
+        textTime.text = FormatTime(Time.time);
     }
 
+    // 초 단위의 time을 분과 초로 나누어서 표기함
+    string FormatTime(float time)
+    {
+        int seconds = Mathf.FloorToInt(time);
+        int minutes = seconds / 60;
+        int remainSeconds = seconds % 60;
+        return $"{minutes} : {remainSeconds:00}";
+    }
     // 게임을 멈춘다.
     private void GameOver()
     {
