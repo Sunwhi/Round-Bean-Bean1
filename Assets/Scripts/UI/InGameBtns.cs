d074f67328001b60316c75b68cac86a2fc374f3c
Sunwhi <sunwhi123@naver.com>
2024-12-23 23:12:07 +0900

    기록 없을 시 메인메뉴 업적창 비활성화
    +업적창 x버튼 위치 수정

--- Assets/Scripts/UI/InGameBtns.cs
+++ Assets/Scripts/UI/InGameBtns.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class CleasRecordBtn : MonoBehaviour
 {
     
     [SerializeField] GameObject ClearRecordPanel;
+    // 업적창 배경 어둡게 하기 위한 Panel
+    [SerializeField] GameObject BackgroundPanel;
+    [SerializeField] Button achieveBtn;
     public void StartGame()
     {
         SceneManager.LoadScene("InGame");
@@ -26,12 +30,34 @@ public class CleasRecordBtn : MonoBehaviour
         {
             ClearRecordPanel.SetActive(true);
         }
+        if (!BackgroundPanel.activeSelf)
+        {
+            BackgroundPanel.SetActive(true);
+        }
     }
     public void ExitBtn()
     {
         if(ClearRecordPanel.activeSelf)
         {
             ClearRecordPanel.SetActive(false);
+        }
+        if (BackgroundPanel.activeSelf)
+        {
+            BackgroundPanel.SetActive(false);
+        }
+    }
+
+    private void Start()
+    {
+        float score = PlayerPrefs.GetFloat("score0");
+        // 기록이 안 남아있을 시 버튼 흐리고 터치 x
+        if (score == 10000f)
+        {
+            achieveBtn.interactable = false; // 버튼 터치 불가, 흐리게 처리
+        }
+        else
+        {
+            achieveBtn.interactable = true; // 버튼 터치 가능
         }
     }
 }
