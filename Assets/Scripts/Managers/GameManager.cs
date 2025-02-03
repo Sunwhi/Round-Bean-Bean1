c6a2f6c39936f6dd11c5b591c8c13ece81da1789
Sunwhi <sunwhi123@naver.com>
2025-02-03 16:28:31 +0900

    업적창 시계 버그 수정
    계절 바뀌는 거리 distance를 지형이 딱 바뀌는 지점으로 수정함.

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -114,7 +114,6 @@ public class GameManager : MonoBehaviour
         {
             if(sfxOnce == 0)
             {
-                Debug.Log("d");
                 SoundManager.Instance.SFXPlay("GameOver",gameOverClip);
                 sfxOnce = 1;
             }
@@ -153,6 +152,7 @@ public class GameManager : MonoBehaviour
         if (animal.transform.position.x > distance)
         {
             distance = animal.transform.position.x - startPos;
+            //Debug.Log(distance);
         }
     }
 
@@ -342,11 +342,11 @@ public class GameManager : MonoBehaviour
     private void CurrentSeason(float distance)
     {
         // 24.12.28 기준 봄 50m, 여름 50m, 가을 55m, 겨울 60m, 봄2 10m
-        if (distance < 50 * 1) recentSeason = 0; // 봄
-        else if (distance < 100 * 1) recentSeason = 1; // 여름
-        else if (distance < 155 * 1) recentSeason = 2; // 가을
-        else if (distance < 215 * 1) recentSeason = 3; // 겨울
-        else if (distance < 225 * 1) recentSeason = 0; // spring2
+        if (distance < 75 * 1) recentSeason = 0; // 봄
+        else if (distance < 125 * 1) recentSeason = 1; // 여름
+        else if (distance < 180 * 1) recentSeason = 2; // 가을
+        else if (distance < 240 * 1) recentSeason = 3; // 겨울
+        else if (distance < 300 * 1) recentSeason = 0; // spring2 - 임시값
 
         PlayerPrefs.SetInt("recentSeason", recentSeason);
     }
